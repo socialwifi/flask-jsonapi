f717047d15cb330e137e5b321845d0d33dfc6dc2
Jakub Trochim <jtrochim@socialwifi.com>
2017-05-02 11:21:02 +0200

    Add library bootstrap.
    Change-Id: Ic71c14f251dad4f0eaa6f9a59294e5212e9d9870

---
+++ README.md
@@ -0,0 +1,13 @@
+# flask-jsonapi
+
+JSONAPI 1.0 server implementation for Flask.
+
+## Running tests
+
+```
+virtualenv -p python3.6 ~/flask-jsonapi-virtualenv
+. ~/flask-jsonapi-virtualenv/bin/activate
+pip install -r base_requirements.txt
+pip install -U pytest==3.0.5
+pytest
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from pip.req import parse_requirements
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='flask-jsonapi',
+    version='0.0.0.dev0',
+    description='JSONAPI 1.0 implementation for Flask.',
+    author='Jakub Trochim',
+    author_email='it@socialwifi.com',
+    url='https://github.com/socialwifi/flask_jsonapi',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[str(ir.req) for ir in parse_requirements('base_requirements.txt', session=False)],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
