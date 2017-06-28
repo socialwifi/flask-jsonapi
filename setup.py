78e35e5e1b136e360cb9a02995e900c1f66229f0
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2017-06-28 09:59:04 +0200

    Back to development: 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='flask-jsonapi',
-    version='0.2.1',
+    version='0.2.2.dev0',
     description='JSONAPI 1.0 implementation for Flask.',
     author='SocialWifi',
     author_email='it@socialwifi.com',
