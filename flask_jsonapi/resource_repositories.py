faec3b4c5ef6b0ef7efe4a69cf7692edd52dc9df
Łukasz Podkalicki <lukasz.podkalicki@socialwifi.com>
2017-10-26 19:13:00 +0200

    Allow to set custom repository view class.

--- flask_jsonapi/resource_repositories.py
+++ flask_jsonapi/resource_repositories.py
@@ -27,48 +27,6 @@ class BaseResourceRepository:
         yield
 
 
-class ResourceRepositoryViewSet:
-    repository = BaseResourceRepository()
-    schema = descriptors.NotImplementedProperty('schema')
-    filter_schema = filters_schema.FilterSchema({})
-    view_decorators = ()
-    view_kwargs = None
-
-    def __init__(self, *, repository=None, schema=None, filter_schema=None, view_decorators=None, view_kwargs=None):
-        if repository:
-            self.repository = repository
-        if schema:
-            self.schema = schema
-        if filter_schema:
-            self.filter_schema = filter_schema
-        if view_decorators:
-            self.view_decorators = view_decorators
-        if view_kwargs:
-            self.view_kwargs = view_kwargs
-
-    def as_detail_view(self, view_name):
-        return self.decorate(
-            ResourceRepositoryDetailView.as_view(view_name, **self.get_views_kwargs())
-        )
-
-    def as_list_view(self, view_name):
-        return self.decorate(
-            ResourceRepositoryListView.as_view(view_name, filter_schema=self.filter_schema, **self.get_views_kwargs())
-        )
-
-    def decorate(self, view):
-        for decorator in self.view_decorators:
-            view = decorator(view)
-        return view
-
-    def get_views_kwargs(self):
-        return {
-            'schema': self.schema,
-            'repository': self.repository,
-            **(self.view_kwargs or {})
-        }
-
-
 class ResourceRepositoryViewMixin:
     repository = BaseResourceRepository()
 
@@ -97,3 +55,51 @@ class ResourceRepositoryListView(ResourceRepositoryViewMixin, resources.Resource
     def create(self, data, **kwargs):
         return self.repository.create(data, **kwargs)
 
+
+class ResourceRepositoryViewSet:
+    repository = BaseResourceRepository()
+    schema = descriptors.NotImplementedProperty('schema')
+    filter_schema = filters_schema.FilterSchema({})
+    detail_view_cls = ResourceRepositoryDetailView
+    list_view_cls = ResourceRepositoryListView
+    view_decorators = ()
+    view_kwargs = None
+
+    def __init__(self, *, repository=None, schema=None, filter_schema=None, detail_view_cls=None, list_view_cls=None,
+                 view_decorators=None, view_kwargs=None):
+        if repository:
+            self.repository = repository
+        if schema:
+            self.schema = schema
+        if filter_schema:
+            self.filter_schema = filter_schema
+        if detail_view_cls:
+            self.detail_view_cls = detail_view_cls
+        if list_view_cls:
+            self.list_view_cls = list_view_cls
+        if view_decorators:
+            self.view_decorators = view_decorators
+        if view_kwargs:
+            self.view_kwargs = view_kwargs
+
+    def as_detail_view(self, view_name):
+        return self.decorate(
+            self.detail_view_cls.as_view(view_name, **self.get_views_kwargs())
+        )
+
+    def as_list_view(self, view_name):
+        return self.decorate(
+            self.list_view_cls.as_view(view_name, filter_schema=self.filter_schema, **self.get_views_kwargs())
+        )
+
+    def decorate(self, view):
+        for decorator in self.view_decorators:
+            view = decorator(view)
+        return view
+
+    def get_views_kwargs(self):
+        return {
+            'schema': self.schema,
+            'repository': self.repository,
+            **(self.view_kwargs or {})
+        }
