c2efd214fb7b46813bf0faf739f4b983c50922b8
Michal Mokrogulski <michal.mokrogulski@socialwifi.com>
2017-10-26 10:32:01 +0200

    Add marshmallow nesting schema.

---
+++ flask_jsonapi/marshmallow_nested_extension/schema.py
@@ -0,0 +1,12 @@
+from marshmallow_jsonapi import fields
+
+
+class IdMappingSchema:
+    __id__ = fields.Str(required=False)
+
+    def dump(self, obj, many=None, update_fields=True, **kwargs):
+        self.id_map = kwargs.pop('id_map')
+        if self.id_map is not None:
+            if hasattr(obj, 'id') and obj.id in self.id_map.keys():
+                obj.__id__ = self.id_map[obj.id]
+        return super().dump(obj, many, update_fields, **kwargs)
