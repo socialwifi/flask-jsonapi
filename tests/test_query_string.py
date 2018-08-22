9fd04330f1705349ecf0ee20bf1e4c78e477becb
Michal Mokrogulski <michal.mokrogulski@socialwifi.com>
2018-08-22 14:23:54 +0200

    Add SortParser.

--- tests/test_query_string.py
+++ tests/test_query_string.py
@@ -1,7 +1,33 @@
 import pytest
+from marshmallow_jsonapi import Schema
+from marshmallow_jsonapi import fields
 
 from flask_jsonapi import exceptions
 from flask_jsonapi import query_string
+
+
+@pytest.fixture
+def user_schema():
+    class UserSchema(Schema):
+        id = fields.Str()
+        name = fields.Str()
+        age = fields.Integer()
+        email_address = fields.Str(attribute='email')
+        role = fields.Relationship()
+
+        class Meta:
+            type_ = 'user'
+            self_view_many = 'user_list'
+            self_view = 'user_detail'
+            self_view_kwargs = {'user_id': '<id>'}
+            strict = True
+
+    return UserSchema
+
+
+@pytest.fixture
+def sort_parser(user_schema):
+    return query_string.SortParser(schema=user_schema)
 
 
 class TestSizeNumberPagination:
@@ -60,3 +86,25 @@ class TestSizeNumberPagination:
                 'next': None,
                 'last': 'http://localhost/examples/?page[size]=10&page[number]=5'
             }
+
+
+class TestSortParser:
+    def test_sorting(self, app, sort_parser):
+        with app.test_request_context('/examples/?sort=name,-age'):
+            parsed_sort = sort_parser.parse()
+        assert parsed_sort == ('name', '-age')
+
+    def test_parser_change_field_name_to_attribute(self, app, sort_parser):
+        with app.test_request_context('/examples/?sort=email_address'):
+            parsed_sort = sort_parser.parse()
+        assert parsed_sort == ('email', )
+
+    def test_parser_raise_exception_when_field_does_not_exist(self, app, sort_parser):
+        with pytest.raises(exceptions.InvalidSort):
+            with app.test_request_context('/examples/?sort=some_bad_field'):
+                sort_parser.parse()
+
+    def test_parser_raise_exception_when_sorting_on_relation_field(self, app, sort_parser):
+        with pytest.raises(exceptions.InvalidSort):
+            with app.test_request_context('/examples/?sort=role'):
+                sort_parser.parse()
