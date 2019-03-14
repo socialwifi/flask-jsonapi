c65da6a1d0de9ac90648a347660faf44577493c2
Behoston <mlegiecki@gmail.com>
2019-03-14 17:38:31 +0100

    Refactor: move permissions to package.

---
+++ flask_jsonapi/permissions/__init__.py
@@ -0,0 +1,19 @@
+from .actions import CREATE_ACTION
+from .actions import DESTROY_ACTION
+from .actions import LIST_ACTION
+from .actions import READ_ACTION
+from .actions import UPDATE_ACTION
+from .checkers import ObjectLevelPermissionChecker
+from .checkers import PermissionChecker
+from .checkers import PermissionException
+
+__all__ = [
+    CREATE_ACTION,
+    READ_ACTION,
+    UPDATE_ACTION,
+    DESTROY_ACTION,
+    LIST_ACTION,
+    PermissionChecker,
+    ObjectLevelPermissionChecker,
+    PermissionException,
+]
