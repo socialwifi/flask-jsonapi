b1c28201283734a5a7969a95beedff40efeba51b
Behoston <mlegiecki@gmail.com>
2019-03-15 10:27:08 +0100

    Add protected views.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,16 @@ import pytest
 def app():
     application = flask.Flask(__name__)
     return application
+
+
+@pytest.fixture
+def api(app):
+    from flask_jsonapi import api
+    application_api = api.Api(app)
+    return application_api
+
+
+@pytest.fixture
+def test_client(app):
+    with app.test_client() as client:
+        yield client
