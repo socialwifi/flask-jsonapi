2f82e6fcc87bc5d479861bf67f850c035104c14c
Jakub Gocławski <jakub@goclawski.net>
2021-05-07 14:22:22 +0200

    Back to development: 0.11.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_long_description():
 
 setup(
     name='flask-jsonapi',
-    version='0.11.0',
+    version='0.11.1.dev0',
     description='JSONAPI 1.0 implementation for Flask.',
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
