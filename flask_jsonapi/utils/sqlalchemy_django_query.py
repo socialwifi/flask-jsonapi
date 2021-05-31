c4e5e0d012ea8a8f3b54ace8525d70623d676b17
Piotr Maliński <piotr.malinski@socialwifi.com>
2021-05-31 11:28:51 +0200

    Update mapper.
    There is also a bug in sqlalchemy-utils forcing a copy of get_mapper function
    https://github.com/kvesteri/sqlalchemy-utils/issues/530

--- flask_jsonapi/utils/sqlalchemy_django_query.py
+++ flask_jsonapi/utils/sqlalchemy_django_query.py
@@ -26,7 +26,7 @@ from sqlalchemy.orm.query import Query
 from sqlalchemy.sql import extract
 from sqlalchemy.sql import operators
 from sqlalchemy.util import to_list
-from sqlalchemy_utils import get_mapper
+from sqlalchemy_utils.functions import orm
 
 from flask_jsonapi import exceptions
 
@@ -113,14 +113,14 @@ class DjangoQueryMixin(object):
                 desc = False
             column = None
             for token in arg.split('__'):
-                column = get_column(self._compile_state()._joinpoint_zero(), token, joins_needed)
+                column = get_column(self._filter_by_zero(), token, joins_needed)
                 if column and column.impl.uses_objects:
                     q = q.join(column)
                     joins_needed.append(column)
                     column = None
             if column is None:
                 raise exceptions.InvalidSort(
-                    "You can't sort on {}".format(token))
+                    "You can't sort on {}, {}".format(token, str(arg)))
             if desc:
                 column = column.desc()
             args[idx] = column
@@ -166,13 +166,83 @@ class DjangoQuery(DjangoQueryMixin, Query):
 
 def get_column(joinpoint, token, joins_needed):
     try:
-        return _entity_descriptor(get_mapper(joinpoint), token)
+        result = _entity_descriptor(get_mapper(joinpoint), token)
+        if type(result) != property:
+            return result
     except exc.InvalidRequestError:
         pass
-    except ValueError:
-        return _entity_descriptor(get_mapper(joinpoint.__table__), token)
     for join in joins_needed:
         try:
             return _entity_descriptor(join, token)
         except exc.InvalidRequestError:
             pass
+
+
+def get_mapper(mixed):
+    """
+    Return related SQLAlchemy Mapper for given SQLAlchemy object.
+
+    :param mixed: SQLAlchemy Table / Alias / Mapper / declarative model object
+
+    ::
+
+        from sqlalchemy_utils import get_mapper
+
+
+        get_mapper(User)
+
+        get_mapper(User())
+
+        get_mapper(User.__table__)
+
+        get_mapper(User.__mapper__)
+
+        get_mapper(sa.orm.aliased(User))
+
+        get_mapper(sa.orm.aliased(User.__table__))
+
+
+    Raises:
+        ValueError: if multiple mappers were found for given argument
+
+    .. versionadded: 0.26.1
+    """
+    if isinstance(mixed, orm._MapperEntity):
+        mixed = mixed.expr
+    elif isinstance(mixed, orm.sa.Column):
+        mixed = mixed.table
+    elif isinstance(mixed, orm._ColumnEntity):
+        mixed = mixed.expr
+
+    if isinstance(mixed, orm.sa.orm.Mapper):
+        return mixed
+    if isinstance(mixed, orm.sa.orm.util.AliasedClass):
+        return orm.sa.inspect(mixed).mapper
+    if isinstance(mixed, orm.sa.sql.selectable.Alias):
+        mixed = mixed.element
+    if isinstance(mixed, orm.AliasedInsp):
+        return mixed.mapper
+    if isinstance(mixed, orm.sa.orm.attributes.InstrumentedAttribute):
+        mixed = mixed.class_
+    if isinstance(mixed, orm.sa.Table):
+        if hasattr(orm.mapperlib, '_all_registries'):
+            all_mappers = set()
+            for mapper_registry in orm.mapperlib._all_registries():
+                all_mappers.update(mapper_registry.mappers)
+        else:  # SQLAlchemy <1.4
+            all_mappers = orm.mapperlib._mapper_registry
+        mappers = [
+            mapper for mapper in all_mappers
+            if mixed in {mapper.local_table}
+        ]
+        if len(mappers) > 1:
+            raise Exception('Still to many mappers %s' % str(mappers))
+        if not mappers:
+            raise ValueError(
+                "Could not get mapper for table '%s'." % mixed.name
+            )
+        else:
+            return mappers[0]
+    if not orm.isclass(mixed):
+        mixed = type(mixed)
+    return orm.sa.inspect(mixed)
