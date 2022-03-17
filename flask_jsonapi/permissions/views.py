e72a9d20c4b4f0cdcc1ab9ef895a7fafed50d197
Jakub Gocławski <jakub@goclawski.net>
2022-03-17 11:05:48 +0100

    Allow overriding resource action methods in protected views.

--- flask_jsonapi/permissions/views.py
+++ flask_jsonapi/permissions/views.py
@@ -14,19 +14,28 @@ class ProtectedDetailView(resource_repository_views.ResourceRepositoryDetailView
         self.permission_checker = permission_checker
 
     def read(self, id):
-        resource = super().read(id)
+        resource = self.protected_read(id)
         resource = self.permission_checker.check_read_permission(resource=resource)
         return resource
 
+    def protected_read(self, id):
+        return super().read(id)
+
     def destroy(self, id):
-        resource = super().read(id)
+        resource = self.protected_read(id)
         resource = self.permission_checker.check_destroy_permission(resource=resource)
+        return self.protected_destroy(resource)
+
+    def protected_destroy(self, resource):
         return super().destroy(resource.id)
 
     def update(self, id, data, **kwargs):
-        resource = super().read(id)
+        resource = self.protected_read(id)
         resource, data = self.permission_checker.check_update_permission(resource=resource, data=data)
-        return super().update(id, data, **kwargs)
+        return self.protected_update(resource, data, **kwargs)
+
+    def protected_update(self, resource, data, **kwargs):
+        return super().update(resource.id, data, **kwargs)
 
 
 class ProtectedListView(resource_repository_views.ResourceRepositoryListView, abc.ABC):
@@ -36,7 +45,7 @@ class ProtectedListView(resource_repository_views.ResourceRepositoryListView, ab
 
     def read_many(self, filters: dict, **kwargs):
         filters = self._apply_permission_filter(filters)
-        resources = super().read_many(filters, **kwargs)
+        resources = self.protected_read_many(filters, **kwargs)
         length_before_permission = len(resources)
         resources = self.permission_checker.check_list_permission(resources=resources)
         if length_before_permission != len(resources):
@@ -47,8 +56,14 @@ class ProtectedListView(resource_repository_views.ResourceRepositoryListView, ab
     def _apply_permission_filter(self, filters: dict) -> dict:
         pass
 
+    def protected_read_many(self, filters, **kwargs):
+        return super().read_many(filters, **kwargs)
+
     def create(self, data: dict, **kwargs):
         data = self.permission_checker.check_create_permission(data=data)
+        return self.protected_create(data, **kwargs)
+
+    def protected_create(self, data: dict, **kwargs):
         return super().create(data, **kwargs)
 
 
