1320ccb08607f294694afbc9df424b513b063c34
Jakub Gocławski <jakub@goclawski.net>
2026-02-02 14:16:09 +0100

    Remove deprecated build options.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
